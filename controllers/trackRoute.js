a886e0b2533aa41baefcba6c9e665da0f0038b58
EBRAHIM-CODE <ebrahim0ramadhan@gmail.com>
2024-08-15 03:23:17 +0300

    final backend done for lapp

--- controllers/trackRoute.js
+++ controllers/trackRoute.js
@@ -5,7 +5,7 @@ const Track = require("../models/track");
 router.post("/", async (req, res) => {
   try {
     const track = await Track.create(req.body);
-    res.status(201).json({ track });
+    res.status(201).json(track);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -14,7 +14,7 @@ router.post("/", async (req, res) => {
 router.get("/", async (req, res) => {
   try {
     const tracks = await Track.find();
-    res.status(200).json({ tracks });
+    res.status(200).json(tracks);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -45,7 +45,7 @@ router.put("/:id", async (req, res) => {
     });
     if (!track) res.status(404).json({ message: "Track not found" });
     else {
-      res.status(200).json({ track });
+      res.status(200).json(track);
     }
   } catch (error) {
     if (res.status(404).json({ error: error.message }));
@@ -60,7 +60,7 @@ router.delete("/:id", async (req, res) => {
     const track = await Track.findByIdAndDelete(req.params.id);
     if (!track) res.status(404).json({ message: "Track not found" });
     else {
-      res.status(200).json({ track });
+      res.status(200).json(track);
     }
   } catch (error) {
     if (res.status(404).json({ error: error.message }));
